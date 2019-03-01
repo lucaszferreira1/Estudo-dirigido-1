60a34eb4da208cfeb34a546a02650e2f7f437b7f
lucaszferreira1 <48135696+lucaszferreira1@users.noreply.github.com>
2019-03-01 16:31:29 -0300

    Resultado Final

--- exercicio_2.php
+++ exercicio_2.php
@@ -1,1 +1,16 @@
 <?php
+
+print "Quantos dias:";
+ $dias=fgets(STDIN);
+print "Quantas horas:";
+ $horas=fgets(STDIN);
+print "Quantos minutos:";
+ $minutos=fgets(STDIN);
+print "Quantos segundos:";
+ $segundos=fgets(STDIN);
+
+$horasfinal= ($dias*24)+$horas;
+$minutosfinal= ($horasfinal*60)+$minutos;
+$segundosfinal= ($minutosfinal*60)+$segundos;
+
+print "São $segundosfinal segundos";
