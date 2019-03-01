89f7eba6d8681bf4e0fbf1225fca8afc5eb77176
lucaszferreira1 <48135696+lucaszferreira1@users.noreply.github.com>
2019-03-01 16:48:06 -0300

    meio quase final

--- exercicio_4.php
+++ exercicio_4.php
@@ -1,1 +1,12 @@
 <?php
+
+print "Qual o preço da mercadoria:";
+  $mercadoria=fgets(STDIN);
+print "Quanto de desconto:";
+  $desconto=fgets(STDIN);
+
+$precodesconto=($mercadoria*($desconto/100));
+$precofinal=$mercadoria-$precodesconto;
+
+print "O desconto foi de $precodesconto";
+print "O preço final é de $precofinal";
