417f1d48827264afaf7209fa999b7e064fc4c88f
lucaszferreira1 <48135696+lucaszferreira1@users.noreply.github.com>
2019-03-01 16:38:27 -0300

    Resultado Final

--- exercicio_3.php
+++ exercicio_3.php
@@ -1,1 +1,12 @@
 <?php
+
+print "Qual o seu salário:";
+$salario=fgets(STDIN);
+print "Qual a porcentagem de aumento:";
+$aumento=fgets(STDIN);
+
+$dinheiroaumento= ($salario*($aumento/100));
+$aumentoreal=$dinheiroaumento+$salario;
+
+print "Você receberá um aumento de $dinheiroaumento";
+print "Seu salário será de $aumentoreal";
