5852b717cc710ef7e9fe3b1c272a42c8923b1448
lucaszferreira1 <48135696+lucaszferreira1@users.noreply.github.com>
2019-03-06 21:45:29 -0300

    em andamento (vai tenta rouba de outro)

--- exercicio_9.php
+++ exercicio_9.php
@@ -1,2 +1,14 @@
 <?php
 
+print "Qual o tamanho da área a ser pintada,em metros quadrados,:";
+$area=(float)fgets(STDIN);
+
+$totallitros=($area/6)*1.1;
+
+$nlatas=$totallitros/18 //arrendondar para cima//;
+$precolatas=$nlatas*80;
+
+$ngaloes=$totallitros/3.6 //arrendondar para cima//;
+$precogaloes=$ngaloes*25;
+
+$nlatasgaloes=($totallitros/18)
