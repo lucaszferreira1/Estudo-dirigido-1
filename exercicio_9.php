c4835c2b49c5043b6c4a3bd641bcb6934616f8c3
lucaszferreira1 <48135696+lucaszferreira1@users.noreply.github.com>
2019-03-06 21:46:38 -0300

    em andamento (vai roubar de outro seu vagabundo)

--- exercicio_9.php
+++ exercicio_9.php
@@ -11,4 +11,4 @@ $precolatas=$nlatas*80;
 $ngaloes=$totallitros/3.6 //arrendondar para cima//;
 $precogaloes=$ngaloes*25;
 
-$nlatasgaloes=($totallitros/18)
+$nlatasgaloes=($totallitros/18) //parte decimal multiplicar por 18 e dividir o resultado por 3.6//;
