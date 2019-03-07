096214c743fb883584829c89cc00a12d2cc56d4a
lucaszferreira1 <48135696+lucaszferreira1@users.noreply.github.com>
2019-03-06 21:15:50 -0300

    meio quase final

---
+++ exercicio_5.php
@@ -0,0 +1,10 @@
+<?php
+
+print "Qual foi a distancia percorrida";
+ $distancia=(float)fgets(STDIN);
+print "Quanto velocidade:";
+ $velocidade=(float)fgets(STDIN);
+
+$tempo=$distancia/$velocidade;
+
+print "Levará/Levou $tempo horas";
