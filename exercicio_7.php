b1439465aa2a8c0930d90f4a6e3b3091a421a21d
lucaszferreira1 <48135696+lucaszferreira1@users.noreply.github.com>
2019-03-11 14:44:49 -0300

    Resultado Final

--- exercicio_7.php
+++ exercicio_7.php
@@ -1,10 +1,7 @@
 <?php
-
 print "Quantos km foram percorridos:";
 $kmpercorridos=(float)fgets(STDIN);
 print "Quantos dias o carro foi alugado:";
 $dias=(int) fgets(STDIN);
-
 $preco=(60*$dias)+(0.15*$kmpercorridos);
-
-print "São $preco Reais"
+print "São $preco Reais \n";
