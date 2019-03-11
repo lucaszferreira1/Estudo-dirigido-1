44a8b66ff90829e783357e6400028021eada7ea1
lucaszferreira1 <48135696+lucaszferreira1@users.noreply.github.com>
2019-03-11 14:43:46 -0300

    Resultado Final

--- exercicio_6.php
+++ exercicio_6.php
@@ -1,6 +1,5 @@
 <?php
-
 print "Quantos graus celsius:";
  $celsius=(float)fgets(STDIN);
 $farenheit=((9*$celsius)/5)+32;
-print "São $farenheit graus farenheit";
+print "São $farenheit graus farenheit \n";
