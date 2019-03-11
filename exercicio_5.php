c164b8ac144d5a7fded9501aedd876b7f9777d85
lucaszferreira1 <48135696+lucaszferreira1@users.noreply.github.com>
2019-03-11 14:39:31 -0300

    Resultado Final

--- exercicio_5.php
+++ exercicio_5.php
@@ -1,10 +1,7 @@
 <?php
-
 print "Qual foi a distancia percorrida";
  $distancia=(float)fgets(STDIN);
 print "Quanto velocidade:";
  $velocidade=(float)fgets(STDIN);
-
 $tempo=$distancia/$velocidade;
-
 print "Levará/Levou $tempo horas";
