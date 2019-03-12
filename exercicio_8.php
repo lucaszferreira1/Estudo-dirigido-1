ad268843dc39f331892e5402c38a14d5977b2926
lucaszferreira1 <48135696+lucaszferreira1@users.noreply.github.com>
2019-03-12 09:39:00 -0300

    Resultado Final

--- exercicio_8.php
+++ exercicio_8.php
@@ -1,1 +1,12 @@
 <?php
+ print "Quantos cigarros você fuma por dia:";
+ $cigarros_diarios = (int)fgets(STDIN);
+
+ print "Quantos anos você fuma:";
+ $anos_fumado = (int)fgets(STDIN);
+ 
+ $cigarros_fumados =($cigarros_diarios)*365*($anos_fumado);
+ $minutos_perdidos = $cigarros_fumados*10;
+ $horas_perdidas   = $minutos_perdidos/60;
+ $dias_perdidos    = round($horas_perdidas / 24);
+ print "Você perdeu $dias_perdidos dias de vida";
