c483f4f020527c4bf50550c8ae877e57809a38d0
lucaszferreira1 <48135696+lucaszferreira1@users.noreply.github.com>
2019-03-15 16:46:54 -0300

    Resultado Final

--- exercicio_9.php
+++ exercicio_9.php
@@ -5,7 +5,9 @@ $area=(float)fgets(STDIN);
 print "Qual método de preço deseja usar(1,2,3):";
 $metodo=(int)fgets(STDIN);
 
-$totallitros=($area/6)*1.1;
+$totallitros=($area/6);
+$latarendimento=(18*6);
+$galaorendimento=(3.6*6);
 
 if ($metodo==1){
 $nlatas=$totallitros/18;
@@ -16,27 +18,18 @@ print "\n Vai dar $precolatas reais \n";}
 
 
 if ($metodo==2){
-$ngaloes=$totallitros/3.6 ;
+$ngaloes=$totallitros/3.6;
 $ngaloes=ceil($ngaloes);
-$precogaloes=$ngaloes*25;$nlatasgaloes=($totallitros/18) //parte decimal multiplicar por 18 e dividir o resultado por 3.6
-;
-$nlatasbaixo=floor($totallitros);
-$partedecimal=$totallitros-$nlatasbaixo;
-$quasengaloes=($partedecimal*18);
-$ngaloes=($quasengaloes)/3.6;
-$nlatas=$partedecimal;
+$precogaloes=$ngaloes*25;
 print "\n São $ngaloes galões";
 print "\n Vai dar $precogaloes reais \n";}
 
 
 if ($metodo==3){
-$nlatasgaloes=($totallitros/18) //parte decimal multiplicar por 18 e dividir o resultado por 3.6
-;
-$nlatasbaixo=floor($totallitros);
-$partedecimal=$totallitros-$nlatasbaixo;
-$quasengaloes=($partedecimal*18);
-$ngaloes=($quasengaloes)/3.6;
-$nlatas=$partedecimal;
-print "$ngaloes galoes e $nlatas latas";
+    $nlatas=(int) $totallitros/($latarendimento);
+    $faltou= $totallitros%($latarendimento);
+    $ngaloes=ceil($faltou/$galaorendimento);
+    $nlatas=ceil($nlatas);
+    print "$nlatas latas - $ngaloes galoes";}
 
-}
+
